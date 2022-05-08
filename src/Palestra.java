5a0ab8aeab6355b29bf231f9fb27385eb474ff2b
HeloContrera <93154949+HeloSilvaC@users.noreply.github.com>
2022-05-07 22:32:20 -0300

    Add files via upload

---
+++ src/Palestra.java
@@ -0,0 +1,13 @@
+
+public class Palestra {
+	String tema;
+	String diaHora;
+	boolean exigeFrequencia;
+
+	public Palestra(String tema, String diaHora, boolean exigeFrequencia) {
+
+		this.tema = tema;
+		this.diaHora = diaHora;
+		this.exigeFrequencia = exigeFrequencia;
+	}
+}
---
+++ src/Participantes.java
@@ -0,0 +1,13 @@
+
+public class Participantes {
+	String[] nome;
+	String[] email;
+	String[] cpf;
+
+	public void Participantes() {
+		this.nome[0] = "Adriana da Silva Contrera";
+		this.email[0] = "Adrianacontrera.seduc@gmail.com";
+		this.cpf[0] = "038.168.039-89";
+	}
+
+}
